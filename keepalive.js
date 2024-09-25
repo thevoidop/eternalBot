7b04bcdc41e19035531256f194b6815f25d29b63
TheVoidOP <119924028+thevoidop@users.noreply.github.com>
2024-09-25 10:25:10 +0530

    Update keepalive.js

--- keepalive.js
+++ keepalive.js
@@ -1,14 +1,16 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("I am up!");
-})
-  
-app.listen(PORT, ()=>{
-  console.log(`Running on Port: ${PORT}`);
-})
+function keepalive() {
+  app.get("/", (req, res) => {
+    res.send("I am up!");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Running on Port: ${PORT}`);
+  });
+}
 
 module.exports = keepalive;
